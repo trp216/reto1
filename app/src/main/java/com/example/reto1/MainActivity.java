31abe2c00a6040a19485a4ff9170787e62d5d64a
Mateo <mateo.loaiza1@correo.icesi.edu.co>
2021-10-17 19:46:17 -0500

    Added a edit profile fragment, not sure how to use it yet

--- app/src/main/java/com/example/reto1/MainActivity.java
+++ app/src/main/java/com/example/reto1/MainActivity.java
@@ -1,14 +1,48 @@
 package com.example.reto1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 public class MainActivity extends AppCompatActivity {
+
+    private ProfileFragment profileFragment;
+    private PostsFragment postsFragment;
+    private BottomNavigationView navigator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        profileFragment = ProfileFragment.newInstance();
+        postsFragment = PostsFragment.newInstance();
+
+        navigator = findViewById(R.id.navigator);
+
+        showFragment(profileFragment);
+
+        navigator.setOnItemSelectedListener(menuItem->{
+
+            if(menuItem.getItemId()==R.id.profileItem){
+                showFragment(profileFragment);
+            }else if(menuItem.getItemId()==R.id.postsItem){
+                showFragment(postsFragment);
+                }
+            return true;
+        });
+
+    }
+
+    public void showFragment(Fragment f){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.fragmentContainer,f);
+        transaction.commit();
     }
 }
--- app/src/main/java/com/example/reto1/ProfileFragment.java
+++ app/src/main/java/com/example/reto1/ProfileFragment.java
@@ -7,11 +7,20 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class ProfileFragment extends Fragment {
 
     //State
 
+//    private ImageView image;
+//    private EditText businessName, description;
+//    private Button editBtn;
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -29,6 +38,7 @@ public class ProfileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false);
     }

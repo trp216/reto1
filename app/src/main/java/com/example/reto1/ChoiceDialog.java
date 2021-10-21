22bf62a489d030e66ed3e3c53c019da9e1f0fec6
Mateo <mateo.loaiza1@correo.icesi.edu.co>
2021-10-21 12:26:24 -0500

    Fixed some design issues and improved  the choice fragment style

--- app/src/main/java/com/example/reto1/ChoiceDialog.java
+++ app/src/main/java/com/example/reto1/ChoiceDialog.java
@@ -2,6 +2,7 @@ package com.example.reto1;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.ListFragment;
@@ -29,12 +30,19 @@ public class ChoiceDialog extends DialogFragment {
     }
 
     @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setStyle(DialogFragment.STYLE_NO_TITLE, R.style.MyDialog);
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_choice_dialog, container, false);
         camBtn = view.findViewById(R.id.camBtn);
         galleryBtn = view.findViewById(R.id.galleryBtn);
+
 
         camBtn.setOnClickListener(v->{
             listener.onChoice(0);

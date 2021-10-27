dee10a4a3287209175848388c45a105dc2f568c9
Alejandra Díaz <47869960+trp216@users.noreply.github.com>
2021-10-26 20:33:21 -0500

    Create Post.java
    Added class Post.

---
+++ app/src/main/java/com/example/reto1/Post.java
@@ -0,0 +1,47 @@
+package com.example.reto1;
+
+public class Post {
+
+    private String name,location,start,end;
+
+    public Post(){}
+
+    public Post(String name, String location, String start, String end) {
+        this.name = name;
+        this.location = location;
+        this.start = start;
+        this.end = end;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getStart() {
+        return start;
+    }
+
+    public void setStart(String start) {
+        this.start = start;
+    }
+
+    public String getEnd() {
+        return end;
+    }
+
+    public void setEnd(String end) {
+        this.end = end;
+    }
+}
